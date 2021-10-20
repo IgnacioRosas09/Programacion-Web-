26b3a04a2fb30b16d5c28986803719310651af61
IgnacioRosas09 <worldrosas99@gmail.com>
2021-10-20 07:43:27 -0500

    Hola Tu

--- Django/hola/Principal/Urls.py
+++ Django/hola/Principal/Urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.holaDjango, name="holaDjango")]
+    path("", views.holaDjango, name="holaDjango"),
+    path("pepe", views.pepe, name="Hola Pepe"),
+    path('<str:nombre>', views.holaTu, name='holaTu')
+    ]
--- Django/hola/Principal/views.py
+++ Django/hola/Principal/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def holaDjango(request):
     return HttpResponse("Hola Django!")
+
+def pepe(request):
+    return HttpResponse("Hola Pepe!")
+
+def holaTu(request, nombre):
+    return HttpResponse('Hola {nombre.capitalize()}!')
