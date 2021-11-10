377e45824ac2a5e61a7998a0b14ff969ed18304a
IgnacioRosas09 <worldrosas99@gmail.com>
2021-11-09 23:08:55 -0600

    alarmas v3.2

--- Django/hola/alarmas/views.py
+++ Django/hola/alarmas/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django import forms
+from django.http import HttpResponseRedirect
+from django.urls import reverse 
 
 alarmas = ['6:50', '7:00']
 # Create your views here.
@@ -8,7 +10,16 @@ def index(request):
     return render(request, 'alarmas/index.html',{'alarmas':alarmas})
 
 def v2(request):
-    return render(request, 'alarmas/v2.html', {'cont_form':FNuevaAlarma()})
+    if request.method == 'POST':
+        form = FNuevaAlarma(request.POST)
+        if form.is_valid():
+            alarma = form.cleaned_data['alarma']
+            alarmas.append(alarma)
+            return HttpResponseRedirect(reverse('alarmas:index'))
+        else:
+            return render(request, 'alarmas/v2.html', {'cont_form': form})
+    else:
+        return render(request, 'alarmas/v2.html', {'cont_form':FNuevaAlarma()})
 
 class FNuevaAlarma(forms.Form):
     alarma = forms.CharField(label='Programar alarma')
