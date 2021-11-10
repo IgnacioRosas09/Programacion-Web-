a61f665031bfb1aa218a048ab77797188c563a2d
IgnacioRosas09 <worldrosas99@gmail.com>
2021-11-10 07:40:53 -0600

    alarmas v4

--- Django/hola/alarmas/views.py
+++ Django/hola/alarmas/views.py
@@ -3,18 +3,17 @@ from django import forms
 from django.http import HttpResponseRedirect
 from django.urls import reverse 
 
-alarmas = ['6:50', '7:00']
-# Create your views here.
-
 def index(request):
-    return render(request, 'alarmas/index.html',{'alarmas':alarmas})
+    if "alarmas" not in request.session:
+        request.session["alarmas"] = ['6:50', '7:00']
+    return render(request, 'alarmas/index.html',{'alarmas':request.session["alarmas"]})
 
 def v2(request):
     if request.method == 'POST':
         form = FNuevaAlarma(request.POST)
         if form.is_valid():
             alarma = form.cleaned_data['alarma']
-            alarmas.append(alarma)
+            request.session["alarmas"] += [alarma]
             return HttpResponseRedirect(reverse('alarmas:index'))
         else:
             return render(request, 'alarmas/v2.html', {'cont_form': form})
