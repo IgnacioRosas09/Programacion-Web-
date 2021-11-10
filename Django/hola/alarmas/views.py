848888d85dbfc1450a9778a8274430b653a05697
IgnacioRosas09 <worldrosas99@gmail.com>
2021-11-09 22:38:06 -0600

    alarmas v3.1

--- Django/hola/alarmas/views.py
+++ Django/hola/alarmas/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django import forms
 
 alarmas = ['6:50', '7:00']
 # Create your views here.
@@ -7,4 +8,8 @@ def index(request):
     return render(request, 'alarmas/index.html',{'alarmas':alarmas})
 
 def v2(request):
-    return render(request, 'alarmas/v2.html')
+    return render(request, 'alarmas/v2.html', {'cont_form':FNuevaAlarma()})
+
+class FNuevaAlarma(forms.Form):
+    alarma = forms.CharField(label='Programar alarma')
+    #snooze = forms.IntegerField(label='Repetir', min_value=0, max_value=0)
